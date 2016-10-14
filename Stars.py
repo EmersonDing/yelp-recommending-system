1afe4a877a5226b204be192274481a0d29622918
Nan Ding <emerson_ding@126.com>
2016-10-14 12:01:21 -0700

    add python star

---
+++ Stars.py
@@ -0,0 +1,31 @@
+"""
+
+This file contains basic mainpulation related to stars table
+"""
+
+import pandas as pd
+pd.set_option('display.mpl_style', 'default')
+
+class Star:
+    def __init__(self):
+        df = pd.read_csv
+
+    ## set path saved with Stars.csv
+    def readCSV(self, path):
+        self.df = pd.read_csv(path)
+
+if __name__ == '__main__':
+    ## initial star object
+    ob = Star()
+
+    ## initial csv file
+    ob.readCSV("/Users/emerson/Documents/Github/yelp_recommending_system/Stars.csv")
+
+    ## print 20th row to 40th
+    # print(ob.df[20:40])
+
+    ## print 20th row to 40th of user_id column
+    # print(ob.df['user_id'][20:40])
+
+    ## print all of the reviews with 4 star
+    print(ob.df[ob.df['stars'].isin([4])])
