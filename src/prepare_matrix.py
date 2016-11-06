1c1d7697a85f2dac0905bea33cc743f53f7c800f
mingo <ipod825@gmail.com>
2016-11-05 22:38:49 -0700

    Add train/test splitting snippet

--- src/prepare_matrix.py
+++ src/prepare_matrix.py
@@ -8,7 +8,9 @@ Script to prepare user-item rating matrix
 import pandas as pd
 import argparse
 import sys
-from scipy import sparse, io
+from scipy import sparse
+import numpy as np
+import pickle
 
 
 def parse_arg(argv):
@@ -18,11 +20,14 @@ def parse_arg(argv):
     parser = argparse.ArgumentParser(description='Prepare rating matrix')
     parser.add_argument('-i', '--inpf', default='../raw_data/Stars_Top_Users.csv', help='csv input file for start matrix')
     parser.add_argument('-o', '--oupf', default='../processed_data/Stars_Top_Users.mtx', help='scipy sparse matrix file containing star matrix')
+    parser.add_argument('-s', '--split', type=float, default=0.1, help='ratio of testing split data, e.g. 0.1 will split 1/10 (usr, business) ratings as testing pairs.')
+    parser.add_argument('-r', '--random_seed', type=int, default=0, help='random seed to split tainging and testing data')
     return parser.parse_args(argv[1:])
 
 
 if __name__ == '__main__':
     args = parse_arg(sys.argv)
+    np.random.seed(args.random_seed)
     df = pd.read_csv(args.inpf)
 
     # extract and user id
@@ -37,9 +42,8 @@ if __name__ == '__main__':
     num_business = len(business_id)
     print('Total unique business id: {}'.format(num_business))
 
-    # construct scipy sparse csr_matrix
-    # Use csc (column based) instead of csr (row based) format. Reference: http://stackoverflow.com/a/16875631
     data = []
+    count= []
     row_ind = []
     col_ind = []
     for _, record in df.iterrows():
@@ -47,11 +51,29 @@ if __name__ == '__main__':
         row_ind.append(uid)
         col_ind.append(bid)
         data.append(rating)
+        count.append(1)
 
-    us_bs_matrix = sparse.csc_matrix((data, (row_ind, col_ind)), shape=(num_user, num_business), dtype=int)
+    # One user might rate one business multiple times, we calculate the avrage for that.
+    count_matrix = sparse.csr_matrix((count, (row_ind, col_ind)), shape=(num_user, num_business), dtype=float)
+    us_bs_matrix = sparse.csr_matrix((data, (row_ind, col_ind)), shape=(num_user, num_business), dtype=float)
 
-    print 'Average rating: ',us_bs_matrix[us_bs_matrix > 0].mean()
+    # Do the average
+    count_matrix.data = 1/count_matrix.data
+    us_bs_matrix = us_bs_matrix.multiply(count_matrix)
 
+    # Split train, test data by randomly picking "split" part of the (user, rating) pairs as testing pairs
+    ub_coo = us_bs_matrix.tocoo()
+    data_row_col = np.random.permutation([[r,c,d] for r,c,d in zip(ub_coo.data, ub_coo.row, ub_coo.col)])
+
+    split = int(args.split * len(data_row_col))
+    test = data_row_col[:split]
+    train = data_row_col[split:]
+
+    train_mat = sparse.coo_matrix((train[:,0], (train[:,1], train[:,2])), shape=(num_user, num_business), dtype=float)
+    test_mat = sparse.coo_matrix((test[:,0], (test[:,1], test[:,2])), shape=(num_user, num_business), dtype=float)
+
+    print("Training pairs: {}".format(train_mat.getnnz()))
+    print("Testing pairs: {}".format(test_mat.getnnz()))
     # output to mtx file
-    print("Saving to {}...".format(args.oupf))
-    io.mmwrite(args.oupf, us_bs_matrix)
+    with open(args.oupf, 'w') as f:
+        pickle.dump({'train':train_mat, 'test': test_mat}, f)
