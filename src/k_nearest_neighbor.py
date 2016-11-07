926ac1c06af5bdc74a86ee91498268a9d8330b6f
mingo <ipod825@gmail.com>
2016-11-07 01:24:41 -0800

    Unifying preprocessing to main.py and migrate movielens example to yelp data

--- src/k_nearest_neighbor.py
+++ src/k_nearest_neighbor.py
@@ -2,31 +2,35 @@
 # -*- coding: utf-8 -*-
 """
 Script to pre-calculate k-nearest neighbor of item in the rating matrix
+Should be merge into model.py/similarity.py
 """
 
 import argparse
 import sys
 
 import numpy as np
-from scipy import io
+import pickle
 
 
 def parse_arg(argv):
     parser = argparse.ArgumentParser(description='Calculate k-neares neighbor of each item')
-    parser.add_argument('-i', '--inpf', default='../processed_data/Stars_30000.mtx', help='input sparse matrix file of the rating matrix')
-    parser.add_argument('-o', '--oupf', default='../processed_data/Stars_30000_neighbors.npy', help='input sparse matrix file of the rating matrix')
+    parser.add_argument('-i', '--inpf', default='../processed_data/Stars_30000.pkl', help='input pickle file containint train/test sparse rating matrix')
+    parser.add_argument('-o', '--oupf', default='../processed_data/Stars_30000.npy', help='')
     return parser.parse_args(argv[1:])
 
 
-def items_pearson_similarity(x):
+def items_pearson_similarity(rating):
     '''
-    @param x: 2D sparse array. Input matrix. shape: (# users, # items).
-    @return 2D non sparse array. correlation coefficient matrix of x. shape: (# items, # items)
+    @param rating: 2D sparse array. Rating matrix. shape: (# users, # items).
+    @return 2D non sparse array. Item similarity matrix. shape: (# items, # items)
     '''
-    covx = column_covariance(x)
+    covx = column_covariance(rating)
     stdx = np.sqrt(np.diag(covx))  # std of each item, shape: (# item,)
     stdx = stdx[np.newaxis,:]  # shape: (1, # of item)
-    return covx/(stdx.T * stdx)
+    res = covx/(stdx.T * stdx)
+    # Note that when one item of the rating matrix is all the same (usually it should be all 0 due to the train/test split process), the std of that column will be 0 and res will be nan for that column.
+    res[np.isnan(res)] = 0
+    return res
 
 
 def column_covariance(x):
@@ -40,14 +44,19 @@ def column_covariance(x):
     return res
 
 
-def shrunk_sim(x, sim_mat):
+def shrunk_sim(rating, sim_mat):
+    '''
+    @param rating: 2D (sparse) array. Rating matrix. shape: (# users, # items)
+    @param sim_mat: 2D non sparse array. Item similarity matrix. shape: (# items, # items)
+    @return: 2D non sparse array. Shrunk version of sim_mat. shape: (# items, # items)
+    '''
     assert sim_mat.shape[0] == sim_mat.shape[1]
 
     lambda2 = 100
     num_item = sim_mat.shape[0]
     rated_users = {}
     for i in range(0, num_item):
-        rated_users[i] = set(x.getcol(i).indices)
+        rated_users[i] = set(rating.getcol(i).indices)
 
     for i in range(i+1, num_item):
         for j in range(i+1, num_item):
@@ -57,9 +66,17 @@ def shrunk_sim(x, sim_mat):
     return sim_mat
 
 
-def knn(rating, k=50):
+def find_item_neighbors(rating, k=50):
+    '''
+    @param rating: 2D (sparse) array. Rating matrix. shape: (# users, # items)
+    @param sim_mat: 2D non sparse array. correlation coefficient of items. shape: (# items, # items)
+    @return: 2D non sparse array. Shrunk version of sim_mat. shape: (# items, # items)
+    '''
     sim_mat = items_pearson_similarity(rating)
+    # TODO check if non-shrunk is worse
     shrunk_sim_mat = shrunk_sim(rating, sim_mat)
+
+    # TODO check if we find neighbors with absolute value could be better. Cause from the view of information gain, most unsimilar item is as informative as most similar item. 
 
     neighbors = np.zeros((sim_mat.shape[0], k), dtype=int)
     for i, row in enumerate(shrunk_sim_mat):
@@ -67,11 +84,23 @@ def knn(rating, k=50):
 
     return neighbors
 
+
+def find_Ru(rating, neighbors):
+    for (i, r_row) in enumerate(rating):
+        print set(r_row.indices).intersection(set(neighbors[i]))
+        sys.exit(-1)
+
+
 if __name__ == '__main__':
     args = parse_arg(sys.argv)
-    rating = io.mmread(args.inpf).tocsc()
+    with open(args.inpf, 'r') as f:
+        rating = pickle.load(f)['train']
 
-    neighbors = knn(rating, k=50)
+    # find neighbor is column based, use csc format
+    neighbors = find_item_neighbors(rating.tocsc(), k=500)
 
-    print("Saving to {}...".format(args.oupf))
-    np.save(args.oupf, neighbors)
+    # # find Ru is row based, use csr format
+    # find_Ru(rating.tocsr(), neighbors)
+
+    # print("Saving to {}...".format(args.oupf))
+    # np.save(args.oupf, neighbors)
---
+++ src/main.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Main script to test different algorithms 
+"""
+
+import argparse
+import os
+import pickle
+import sys
+
+import numpy as np
+import pandas as pd
+import similarity
+from model import Simple_sim, Topk
+from scipy import sparse
+
+
+def parse_arg(argv):
+    '''
+    parsing cli arguments
+    '''
+    parser = argparse.ArgumentParser(description='Prepare rating matrix')
+    parser.add_argument('-i', '--inpf', default='../processed_data/Stars_Top_Users.pkl', help='pickle file containing train/test rating matrix')
+    parser.add_argument('-ri', '--raw_inpf', default='../raw_data/Stars_Top_Users.csv', help='csv file for rating matrix, used to genrate inpf')
+    parser.add_argument('-s', '--split', type=float, default=0.1, help='ratio of testing split data, e.g. 0.1 will split 1/10 (usr, item) ratings as testing pairs.')
+    parser.add_argument('-r', '--random_seed', type=int, default=0, help='random seed to split tainging and testing data')
+    return parser.parse_args(argv[1:])
+
+
+def read_data(df):
+    user_id = {uid: i for i, uid in enumerate(set(df["user_id"]))}
+    item_id = {iid: i for i, iid in enumerate(set(df["business_id"]))}
+
+    n_users = len(user_id)
+    n_items = len(item_id)
+
+    ratings = []
+    count= []
+    row_ind = []
+    col_ind = []
+    for _, row in df.iterrows():
+        row_ind.append(user_id[row[0]])
+        col_ind.append(item_id[row[1]])
+        ratings.append(row[2])
+        count.append(1)
+
+    # One user might rate one item multiple times, we calculate the avrage for that.
+    count_mat = sparse.csr_matrix((count, (row_ind, col_ind)), shape=(n_users, n_items), dtype=float)
+    ui_mat = sparse.csr_matrix((ratings, (row_ind, col_ind)), shape=(n_users, n_items), dtype=float)
+
+    # Do the average
+    count_mat.data = 1/count_mat.data
+    ui_mat = ui_mat.multiply(count_mat)
+
+    return ui_mat
+
+
+def train_test_split(ui_mat, split=0.1):
+    train_mat = ui_mat.copy()
+    test_mat = ui_mat.copy()
+    for i in range(ui_mat.shape[0]):
+        cols = ui_mat.getrow(i).indices
+        cols = np.random.permutation(cols)
+        cut = int(split*len(cols))
+        test_ind = cols[:cut]
+        train_ind = cols[cut:]
+
+        test_mat[i, train_ind] = 0
+        train_mat[i, test_ind] = 0
+
+    train_mat.eliminate_zeros()
+    test_mat.eliminate_zeros()
+
+    # Training and testing should be disjoint
+    assert((train_mat.multiply(test_mat)).sum() == 0)
+    return train_mat, test_mat
+
+
+def preprocess_data(fname):
+    # TODO: Some user id are stange, "(Also" is a use id?
+    df = pd.read_csv(fname)
+    # print df.head()
+
+    user_id = {uid: i for i, uid in enumerate(set(df["user_id"]))}
+    item_id = {iid: i for i, iid in enumerate(set(df["business_id"]))}
+
+    n_users = len(user_id)
+    n_items = len(item_id)
+    ratings = []
+    count= []
+    row_ind = []
+    col_ind = []
+    for _, row in df.iterrows():
+        row_ind.append(user_id[row[0]])
+        col_ind.append(item_id[row[1]])
+        ratings.append(row[2])
+        count.append(1)
+
+    # One user might rate one item multiple times, we calculate the avrage for that.
+    count_mat = sparse.csr_matrix((count, (row_ind, col_ind)), shape=(n_users, n_items), dtype=float)
+    ui_mat = sparse.csr_matrix((ratings, (row_ind, col_ind)), shape=(n_users, n_items), dtype=float)
+
+    # Do the average
+    count_mat.data = 1/count_mat.data
+    ui_mat = ui_mat.multiply(count_mat)
+
+    return ui_mat
+
+
+def get_mse(pred, actual):
+    pred = np.asarray(pred).flatten()
+    actual = np.asarray(actual[actual.nonzero()]).flatten()
+    assert(len(pred)==len(actual))
+    return np.sum((pred-actual)**2)/len(pred)
+
+
+def doIt(modelCls, **model_args):
+    user_model = modelCls(item_based=False, **model_args)
+    item_model = modelCls(item_based=True, **model_args)
+
+    user_model.train(train_mat)
+    item_model.train(train_mat)
+
+    user_prediction = user_model.predict(train_mat, test_mat)
+    item_prediction = item_model.predict(train_mat, test_mat)
+    print
+    print('='*20)
+    print('Model: {}'.format(modelCls.__name__))
+    print('Args: {}'.format(model_args))
+    print('User-based CF MSE: {}'.format(get_mse(user_prediction, test_mat)))
+    print('Item-based CF MSE: {}'.format(get_mse(item_prediction, test_mat)))
+
+if __name__ == '__main__':
+
+    args = parse_arg(sys.argv)
+    np.random.seed(args.random_seed)
+
+    if not os.path.isfile(args.inpf):
+        ui_mat = preprocess_data(args.raw_inpf)
+        train_mat, test_mat = train_test_split(ui_mat, 0.1)
+        with open(args.inpf, 'w') as f:
+            print("Saving train/test matrix ..")
+            print("{} users".format(ui_mat.shape[0]))
+            print("{} items".format(ui_mat.shape[1]))
+            print("{} training pairs".format(train_mat.getnnz()))
+            print("{} testing pairs".format(test_mat.getnnz()))
+
+            pickle.dump({'train':train_mat, 'test': test_mat}, f)
+    else:
+        with open(args.inpf, 'r') as f:
+            data = pickle.load(f)
+            train_mat, test_mat = data['train'], data['test']
+
+    # Note: simple_sim is just TopK with k=\infty
+    doIt(Simple_sim, sim_fn=similarity.cosine_sim)
+    # doIt(Topk, k=50, sim_fn=similarity.cosine_sim)
