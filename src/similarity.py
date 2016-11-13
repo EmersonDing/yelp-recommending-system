191f6c46b9d41b14317b0f9fcd685aead97ca3e4
mingo <ipod825@gmail.com>
2016-11-13 01:42:43 -0800

    Move k_nearest_neighbor.py to simlarity

--- src/similarity.py
+++ src/similarity.py
@@ -1,8 +1,48 @@
 import numpy as np
 
 
-def cosine_sim(ui_mat, epsilon=1e-9):
-    sim = ui_mat.dot(ui_mat.T)
+def cosine_sim(x, epsilon=1e-9):
+    '''
+    @param x: 2D (sparse) array. Input matrix. shape: (# rows, # cols)
+    @return: 2D non sparse array. cosin similarity of each row in x. shape: (# rows, # rows)
+    '''
+    sim = x.dot(x.T)
     sim = sim.toarray() + epsilon
     norms = np.array([np.sqrt(np.diagonal(sim))])
     return (sim / norms / norms.T)
+
+
+def pearson_sim(x):
+    '''
+    @param x: 2D (sparse) array. Input matrix. shape: (# rows, # cols)
+    @return: 2D non sparse array. correlation coefficient of each row in x. shape: (# rows, # rows)
+    '''
+    num_cols = float(x.shape[1])
+    meanx = x.sum(axis=1)/num_cols
+    covx = np.array((x*x.T)/num_cols - meanx*meanx.T)
+
+    stdx = np.sqrt(np.diag(covx))  # std of each item, shape: (# rows,)
+    stdx = stdx[:, np.newaxis]  # shape: (# rows, 1)
+    res = covx/(stdx * stdx.T)
+    # # Note that when one item of the rating matrix is all the same (usually it should be all 0 due to the train/test split process), the std of that column will be 0 and res will be nan for that column.
+    # res[np.isnan(res)] = 0
+
+    return res
+
+
+def shrunk_sim(x, sim_mat, lambda2=100):
+    assert sim_mat.shape[0] == sim_mat.shape[1]
+
+    sim_mat = sim_mat.copy()
+    num_row = sim_mat.shape[0]
+    on_cols = {}
+    for i in range(0, num_row):
+        on_cols[i] = set(x.getcol(i).indices)
+
+    for i in range(0, num_row):
+        for j in range(i+1, num_row):
+            nij = len(on_cols[i].intersection(on_cols[j]))
+            sim_mat[i][j] *= (float(nij)/(nij+lambda2))
+            sim_mat[j][i] *= (float(nij)/(nij+lambda2))
+
+    return sim_mat
---
+++ src/test/test_similarity.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy.testing import assert_allclose
+from scipy.sparse import csr_matrix
+
+from ..similarity import cosine_sim, pearson_sim, shrunk_sim
+
+
+def test_cosine_sim():
+    x = np.array([[1,0,2], [4,5,0]])
+    x = x/np.linalg.norm(x, axis=1, keepdims=True)
+    npres = np.ones((2,2))
+    npres[0,1] = npres[1,0] = np.dot(x[0], x[1])
+    assert_allclose(npres,
+                    cosine_sim(csr_matrix(x)))
+
+
+def test_pearson_similarity():
+    x = np.array([[1,0,2], [4,5,0]])
+    npres = np.corrcoef(x)
+    assert_allclose(npres,
+                    pearson_sim(csr_matrix(x)))
+
+
+def test_shrunk_sim():
+    x = np.array([[1,0,2], [4,5,0]])
+    lambda2 = 100.0
+    sim_mat = np.corrcoef(x)
+    npres = sim_mat.copy()
+    npres[0,1] *= (1/(1+lambda2))
+    npres[1,0] *= (1/(1+lambda2))
+    assert_allclose(npres,
+                    shrunk_sim(csr_matrix(x), sim_mat, lambda2))
